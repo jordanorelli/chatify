e8afd42a915730cb74a17142f046f44339f4acf7
Jordan Orelli <jordanorelli@gmail.com>
2011-11-10 14:32:02 -0500

    broke js out into separate file.

---
+++ static/js/chat.js
@@ -0,0 +1,80 @@
+var since_timestamp = 0;
+
+function addmsg(type, msg) {
+  var chat_message = '';
+  for (var i = 0; i <  msg.messages.length; i++) {
+    chat_message = msg.messages[i];
+    $("#messages").prepend(
+      "<div class='msg "+ type +"'>"+  chat_message.nickname + ": " + chat_message.message +"</div>"
+    );
+  }
+  return  chat_message.timestamp;
+}
+
+function waitForMsg() {
+  $.ajax({
+    type: "GET",
+    url: "/feed",
+    async: true,
+    cache: false,
+    timeout:50000,
+    data: 'since_timestamp=' + since_timestamp,
+    success: function(data) {
+      since_timestamp = addmsg("new", data);
+      setTimeout('waitForMsg()', 1000);
+    },
+
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      addmsg("error", textStatus + " (" + errorThrown + ")");
+      setTimeout('waitForMsg()', "15000");
+    },
+  });
+};
+
+$(document).ready(function(){
+  waitForMsg();
+  var $nickField = $('#nickname');
+  var $messageBox = $('#message');
+
+  $nickField.focus();
+
+  $nickField.keypress(function(event){
+    if($(this).val()!=''){
+      $messageBox.removeAttr("disabled");
+      $('#send').removeAttr("disabled");
+    } else {
+      $messageBox.attr("disabled", "disabled");
+      $('#send').attr("disabled", "disabled");
+    }
+  });
+
+  $nickField.change(function(event){
+    if($(this).val()!=''){
+      $(this).attr("disabled", "disabled");
+      $messageBox.focus();
+    }
+  });
+
+  $('#send').click(function(event){
+    $this = $(this);
+    if($messageBox.val()!=''){
+      var message = $messageBox.val();
+      var nickname = $('#nickname').val();
+      // disable changing our name
+      $this.attr("disabled", "disabled");
+      // enable sending new messages
+      $messageBox.attr("disabled","disabled");
+      $.ajax({
+        type: 'POST',
+        url: '/feed',
+        data: 'nickname=' + nickname + '&message=' + message,
+        success: function(){ $messageBox.val("");  },
+        error: function(){ alert("unable to send message!");},
+        complete: function(){
+          $messageBox.removeAttr("disabled");
+          $('#send').removeAttr("disabled");
+        }
+      });
+    }
+  });
+});
